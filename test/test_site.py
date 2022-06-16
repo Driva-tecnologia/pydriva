737fdae88e29b974e9350322229030b103272e91
Gustavo Guedes <gustavoipguedes@gmail.com>
2022-06-15 21:42:52 -0300

    test: site functions tests

---
+++ test/test_site.py
@@ -0,0 +1,94 @@
+import unittest
+import driva 
+
+class TestValidateSite(unittest.TestCase):
+    def test_site_simple(self):
+        site = 'http://www.google.com'
+        self.assertTrue(driva.site.validate(site))
+    def test_site_without_schema(self):
+        site = 'www.google.com'
+        self.assertTrue(driva.site.validate(site))
+    def test_site_with_path(self):
+        site = 'http://www.google.com/path'
+        self.assertTrue(driva.site.validate(site))
+    def test_site_with_params(self):
+        site = 'http://www.google.com?param=1'
+        self.assertTrue(driva.site.validate(site))
+    def test_site_with_params_and_path(self):
+        site = 'http://www.google.com/path?param=1'
+        self.assertTrue(driva.site.validate(site))
+    def test_br_site(self):
+        site = 'http://www.google.com.br'
+        self.assertTrue(driva.site.validate(site))
+    def test_invalid_tld(self):
+        site = 'http://www.google.com.idontexist'
+        self.assertFalse(driva.site.validate(site))
+
+class TestExtractHost(unittest.TestCase):
+    def test_site_simple(self):
+        site = 'http://www.driva.com.br'
+        self.assertEqual(driva.site.extract_host(site), 'driva.com.br')
+    def test_site_without_schema(self):
+        site = 'www.driva.com.br'
+        self.assertEqual(driva.site.extract_host(site), 'driva.com.br')
+    def test_site_only_domain(self):
+        site = 'driva.com.br'
+        self.assertEqual(driva.site.extract_host(site), 'driva.com.br')
+    def test_site_with_subdomain(self):
+        site = 'http://app.driva.com.br'
+        self.assertEqual(driva.site.extract_host(site), 'app.driva.com.br')
+    def test_complete_site(self):
+        site = 'http://www.driva.com.br/path/to/file?param=1#hash'
+        self.assertEqual(driva.site.extract_domain(site), 'driva.com.br')
+    def test_invalid_site(self):
+        site = 'http://www.driva.com.idontexist'
+        self.assertEqual(driva.site.extract_host(site, raise_if_invalid=False), None)
+    def test_without_tld(self):
+        site = 'http://www.driva'
+        self.assertEqual(driva.site.extract_host(site, raise_if_invalid=False), None)
+
+class TestExtractDomain(unittest.TestCase):
+    def test_site_simple(self):
+        site = 'http://www.driva.com.br'
+        self.assertEqual(driva.site.extract_domain(site), 'driva.com.br')
+    def test_site_without_schema(self):
+        site = 'www.driva.com.br'
+        self.assertEqual(driva.site.extract_domain(site), 'driva.com.br')
+    def test_site_only_domain(self):
+        site = 'driva.com.br'
+        self.assertEqual(driva.site.extract_domain(site), 'driva.com.br')
+    def test_site_with_subdomain(self):
+        site = 'http://app.driva.com.br'
+        self.assertEqual(driva.site.extract_domain(site), 'driva.com.br')
+    def test_complete_site(self):
+        site = 'http://www.driva.com.br/path/to/file?param=1#hash'
+        self.assertEqual(driva.site.extract_domain(site), 'driva.com.br')
+    def test_invalid_site(self):
+        site = 'http://www.driva.com.idontexist'
+        self.assertEqual(driva.site.extract_domain(site, raise_if_invalid=False), None)
+    def test_invalid_site_without_tld(self):
+        site = 'http://www.driva'
+        self.assertEqual(driva.site.extract_domain(site, raise_if_invalid=False), None)
+
+class TestExtractTLD(unittest.TestCase):
+    def test_site_simple(self):
+        site = 'http://www.driva.com.br'
+        self.assertEqual(driva.site.extract_tld(site), 'com.br')
+    def test_site_without_schema(self):
+        site = 'www.driva.com.br'
+        self.assertEqual(driva.site.extract_tld(site), 'com.br')
+    def test_site_only_domain(self):
+        site = 'driva.com.br'
+        self.assertEqual(driva.site.extract_tld(site), 'com.br')
+    def test_site_with_subdomain(self):
+        site = 'http://app.driva.com.br'
+        self.assertEqual(driva.site.extract_tld(site), 'com.br')
+    def test_complete_site(self):
+        site = 'http://www.driva.com.br/path/to/file?param=1#hash'
+        self.assertEqual(driva.site.extract_tld(site), 'com.br')
+    def test_com_site(self):
+        site = 'http://www.google.com'
+        self.assertEqual(driva.site.extract_tld(site), 'com')
+    def test_mexican_site(self):
+        site = 'http://www.google.com.mx'
+        self.assertEqual(driva.site.extract_tld(site), 'com.mx')
