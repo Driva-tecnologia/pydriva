68eae8ecbc122b3a5a1adf30adcf6ec3a9973b8f
Gustavo Guedes <gustavoipguedes@gmail.com>
2023-10-05 14:30:34 +0100

    feat: azure storage shortcuts

--- pydriva/azurestorage.py
+++ pydriva/azurestorage.py
@@ -1,80 +1,121 @@
 
-from azure.storage.filedatalake import (
-    DataLakeServiceClient,
-)
+from azure.storage.filedatalake import DataLakeServiceClient
+from azure.core.exceptions import ResourceNotFoundError
 import os
+from tqdm import tqdm
 
 class AzureStorage:
-    def __init__(self, account_name, credential):
-        account_url = f'https://{account_name}.dfs.core.windows.net'
-        self.service_client = DataLakeServiceClient(
-            account_url=account_url,
-            credential=credential
-        )
+    def __init__(self, conn_str):
+        self.service_client = DataLakeServiceClient.from_connection_string(conn_str=conn_str)
 
-    def get_directory_contents(self, container_name, directory_name):
-        file_system_client = self.service_client.get_file_system_client(file_system=container_name)
-        paths = file_system_client.get_paths(path=directory_name, recursive=False)
+    ########### PUBLIC METHODS ############
+
+    def upload(self, container, path, local_path):
+        if not os.path.exists(local_path):
+            raise Exception(f'{local_path} does not exists')
+        elif os.path.isfile(local_path):
+            return self._upload_file(container, path, local_path)
+        elif os.path.isdir(local_path):
+            return self._upload_path(container, path, local_path)
+        raise Exception(f'{local_path} is not a file or a directory')
         
-        return paths
+    def check_exists(self, container, path):
+        return self._check_exists(container, path)
+
+    def download(self, container, path, local_path='.', log=True):
+        local_path = os.path.join(local_path, os.path.basename(path))
+
+        return self._download_file(container, path, local_path, log)
+        # return self._download_path(container, path, log)
     
-    def download_directory(self, container_name, directory_name, local_path, overwrite=False) -> bool:
-        if not os.path.exists(local_path):
-            os.makedirs(local_path)
+    def delete(self, container, path):
+        if self._check_exists(container, path):
+            return self._delete(container, path)
+        return False
+    
+
+    ########### PRIVATE METHODS ############
+
+    ########### UPLOAD ############
+    def _upload_file(self, container, path, local_path):
+        local_dir = os.path.dirname(local_path)
+        local_filename = local_path.replace(local_dir, '').replace('/', '')
         
-        if not directory_name.endswith('/'):
-            directory_name += '/'
+        az_dir = os.path.dirname(path)
+        az_filename = path.replace(az_dir, '').replace('/', '')
+
+        if az_filename == '':
+            az_filename = local_filename
+
+        file_system_client = self.service_client.get_file_system_client(file_system=container)
+
+        directory_client = file_system_client.get_directory_client(az_dir)
+        file_client = directory_client.create_file(az_filename)
         
-        print(f'Downloading {directory_name} files to {local_path}')
-
-        for path in self.get_directory_contents(container_name, directory_name):
-            if path.is_directory:
-                directory_name = path.name.replace(directory_name, '')
-
-                self.download_directory(
-                    container_name = container_name, 
-                    directory_name = os.path.join(directory_name, directory_name), 
-                    local_path = os.path.join(local_path, directory_name),
-                    overwrite = overwrite
-                )
-
-            else:
-                file_name = path.name.replace(directory_name, '')
-                self.download_file(
-                    container_name = container_name,
-                    directory_name = directory_name,
-                    file_name = file_name,
-                    local_path = local_path,
-                    overwrite = overwrite
-                )
-            
-
-    def download_file(self, container_name, directory_name, file_name, local_path='.', overwrite=False) -> bool:
-        file_system_client = self.service_client.get_file_system_client(file_system=container_name)
-        directory_client = file_system_client.get_directory_client(directory_name)
-        file_client = directory_client.get_file_client(file_name)
-
-        if not os.path.exists(local_path):
-            os.makedirs(local_path)
+        with open(local_path, "rb") as data:
+            file_client.upload_data(data=data, overwrite=True)
         
-        if os.path.exists(os.path.join(local_path, file_name)) and not overwrite:
-            print(f'File {file_name} already exists. Use overwrite=True to overwrite.')
-            return False
-
-        print(f'Downloading {file_name} to {local_path}')
-
-        download = file_client.download_file()
-        with open(os.path.join(local_path, file_name), "wb") as download_file:
-            download_file.write(download.readall())
-
         return True
 
-    def upload_file(self, container_name, directory_name, local_path, file_name, overwrite=True):
-        file_system_client = self.service_client.get_file_system_client(file_system=container_name)
-        directory_client = file_system_client.get_directory_client(directory_name)
-        file_client = directory_client.get_file_client(file_name)
+    def _upload_path(self, container, path, local_path):
+        for file in tqdm(os.listdir(local_path)):
+            if os.path.isfile(os.path.join(local_path, file)):
+                self._upload_file(
+                    container=container,
+                    path=os.path.join(path, file),
+                    local_path=os.path.join(local_path, file)
+                )
+            elif os.path.isdir(os.path.join(local_path, file)):
+                self._upload_path(
+                    container=container,
+                    path=os.path.join(path, file),
+                    local_path=os.path.join(local_path, file)
+                )
+        return True
 
-        print(f'Uploading {file_name} from {local_path} to {directory_name}')
-        with open(file=os.path.join(local_path, file_name), mode="rb") as data:
-            file_client.upload_data(data=data, overwrite=overwrite)
+    ########### CHECK EXISTS ############
+    def _check_exists(self, container, path):
+        file_system_client = self.service_client.get_file_system_client(file_system=container)
+        
+        exists = file_system_client.get_file_client(path).exists()
+        return exists
 
+    ########### DOWNLOAD ############
+    def _download_file(self, container, path, local_path, log=True):
+        file_system_client = self.service_client.get_file_system_client(file_system=container)
+
+
+        if not file_system_client.get_file_client(path).exists():
+            raise ResourceNotFoundError(f'{path} does not exists')
+
+        blob_list = [blob.name for blob in file_system_client.get_paths(path=path, recursive=True) if blob.is_directory == False]
+
+        for blob in tqdm(blob_list, desc='Download'):
+            blob_dir = os.path.dirname(blob).replace(path, '', 1).replace('/', '', 1)
+            local_dir = os.path.join(local_path, blob_dir)
+            if local_dir != '':
+                os.makedirs(local_dir, exist_ok=True)
+            file_client = file_system_client.get_file_client(blob)
+            download = file_client.download_file()
+
+            filename = os.path.join(local_dir, os.path.basename(blob))
+            with open(filename, "wb") as download_file:
+                download_file.write(download.readall())
+        
+        return True
+            
+    ########### DELETE ############
+    def _delete(self, container, path):
+        file_system_client = self.service_client.get_file_system_client(file_system=container)
+        file_client = file_system_client.get_file_client(path)
+        directory_client = file_system_client.get_directory_client(path)
+
+        try:
+            directory_client.delete_directory()
+        except ResourceNotFoundError:
+            print(f'Path {path} not found')
+            raise ResourceNotFoundError(f'Path {path} not found')
+        return True
+
+    ########### OTHERS ############
+    
