93acde27afde3c7a3c82433a1476dc9d4e31d6a4
Gustavo Guedes <gustavoipguedes@gmail.com>
2023-12-12 14:21:30 +0000

    update: upload and download with azcopy

--- pydriva/azurestorage.py
+++ pydriva/azurestorage.py
@@ -1,34 +1,30 @@
 
 from azure.storage.filedatalake import DataLakeServiceClient
 from azure.storage.blob import BlobClient, BlobServiceClient
-from azure.core.exceptions import ResourceNotFoundError
 import os
 from tqdm import tqdm
 from pathlib import Path
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from azcopy_wrapper.azcopy_client import AzClient
+from azcopy_wrapper.azcopy_utilities import AzCopyOptions
 
 class AzureStorage:
     def __init__(self, conn_str):
         self.credential = conn_str
         self.service_client = DataLakeServiceClient.from_connection_string(conn_str=conn_str)
+        self.azcopy = AzClient()
+
+        self.storage_account_name = conn_str.split(';')[1].split('=')[1]
 
     ########### PUBLIC METHODS ############
 
     def upload(self, container, src, dest):
-        if not os.path.exists(src):
-            raise Exception(f'{src} does not exists')
-        elif os.path.isfile(src):
-            return self._upload_file(container, src, dest)
-        elif os.path.isdir(src):
-            return self._upload_path(container, src, dest)
-        raise Exception(f'{src} is not a file or a directory')
+        return self._upload(container, src, dest)
         
     def exists(self, container, blob_name):
         return self._exists(container, blob_name)
 
     def download(self, container, src, dest):
         return self._download(container, src, dest)
-        # return self._download_path(container, path, log)
     
     def delete(self, container, path):
         if self.exists(container, path):
@@ -42,61 +38,41 @@ class AzureStorage:
     ########### PRIVATE METHODS ############
 
     ########### UPLOAD ############
-    def _upload_file(self, container, src, dest):
-        filename = os.path.basename(src)
-
-        if os.path.basename(dest) == '':
-            blob_filename = os.path.join(dest, filename)
-        else:
-            blob_filename = dest
-        
-        blob = BlobClient.from_connection_string(conn_str=self.credential, container_name=container, blob_name=blob_filename)
-        with open(src, "rb") as data:
-            blob.upload_blob(data, overwrite=True)
-        
-        return True
-
-    def _upload_path(self, container, src, dest):
-        basename = os.path.basename(src)
-        all_files = [file.name for file in Path(src).rglob('*') if file.is_file()]
-
-        for file in tqdm(all_files, desc='Upload'):
-            _src = os.path.join(src, file)
-            _dest = os.path.join(dest, basename, file)
-            
-            self._upload_file(container, _src, _dest)
-
-        return True
-
+    def _upload(self, container, src, dest):
+        dest = f'https://{self.storage_account_name}.blob.core.windows.net/{container}/{dest}'
+        try:
+            self.azcopy.upload_data_to_remote_location(
+                src=src,
+                dest=dest,
+                transfer_options= AzCopyOptions(
+                    recursive=True,
+                    overwrite_existing=True
+                ))
+            return True
+        except Exception as e:
+            print(f'Login AzCopy!')
+            return False
     ########### CHECK EXISTS ############
     def _exists(self, container, blob_name):
         return BlobClient.from_connection_string(conn_str=self.credential, container_name=container, blob_name=blob_name).exists()
 
     ########### DOWNLOAD ############
-    def _download_file(self, container, src, dest):
-        filename = os.path.join(dest, os.path.basename(src))
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        blob = BlobClient.from_connection_string(conn_str=self.credential, container_name=container, blob_name=src)
-        blob.download_blob().readinto(open(filename, 'wb'))
+    def _download(self, container, src, dest):
+        src = f'https://{self.storage_account_name}.blob.core.windows.net/{container}/{src}'
 
-        return True
-
-
-    def _download(self, container, src, dest, log=True):
-        dest = os.path.join(dest, os.path.basename(src))
-        blobs = self._list_blobs(container, src)
-
-        if not src.endswith('/'):
-            src += '/'
-
-        for blob in tqdm(blobs, desc='Download'):
-            # remove the src from the blob name
-            _src = blob.replace(src, '')
-            _dest = os.path.join(dest, os.path.dirname(_src))
-            self._download_file(container, blob, _dest)
-            
-        return True
-            
+        try:
+            self.azcopy.download_data_to_local_location(
+                src=src,
+                dest=dest,
+                transfer_options= AzCopyOptions(
+                    recursive=True,
+                    overwrite_existing=True
+                ))
+            return True
+        except Exception as e:
+            print(f'Login AzCopy!')
+            return False
+        
     ########### DELETE ############
     def _delete(self, container, path):
         blob = BlobClient.from_connection_string(conn_str=self.credential, container_name=container, blob_name=path)
@@ -119,16 +95,3 @@ class AzureStorage:
 
         return [blob for blob in blobs if '.' in blob]
     ########### OTHERS ############
-
-
-if __name__ == '__main__':
-    from dotenv import load_dotenv
-    load_dotenv()
-    conn_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
-    az = AzureStorage(conn_str=conn_str)
-
-    az.download(
-        container='tmp',
-        src='pydriva/test_dir',
-        dest='test_pydriva'
-    )
