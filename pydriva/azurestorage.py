6801bcf66c2bb7162fdc4cc19b7ad546f72d207a
Gustavo Guedes <gustavoipguedes@gmail.com>
2024-02-02 16:53:48 +0000

    feat: upload and download with azcopy

--- pydriva/azurestorage.py
+++ pydriva/azurestorage.py
@@ -6,11 +6,14 @@ import os
 from tqdm import tqdm
 from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from azcopy_wrapper.azcopy_client import AzClient
+from azcopy_wrapper.azcopy_client import AzCopyOptions
 
 class AzureStorage:
     def __init__(self, conn_str):
         self.credential = conn_str
         self.service_client = DataLakeServiceClient.from_connection_string(conn_str=conn_str)
+        self.az = AzClient()
 
     ########### PUBLIC METHODS ############
 
@@ -20,7 +23,8 @@ class AzureStorage:
         elif os.path.isfile(src):
             return self._upload_file(container, src, dest)
         elif os.path.isdir(src):
-            return self._upload_path(container, src, dest)
+            return self._upload(container, src, dest)
+        
         raise Exception(f'{src} is not a file or a directory')
         
     def exists(self, container, blob_name):
@@ -42,61 +46,22 @@ class AzureStorage:
     ########### PRIVATE METHODS ############
 
     ########### UPLOAD ############
-    def _upload_file(self, container, src, dest):
-        filename = os.path.basename(src)
-
-        if os.path.basename(dest) == '':
-            blob_filename = os.path.join(dest, filename)
-        else:
-            blob_filename = dest
+    def _upload(self, container, src, dest):
+        transfer_option = AzCopyOptions(overwrite_existing=True, recursive=True)
+        job_info = self.az.upload_data_to_remote_location(src=src, dest=dest, transfer_options=transfer_option)
         
-        blob = BlobClient.from_connection_string(conn_str=self.credential, container_name=container, blob_name=blob_filename)
-        with open(src, "rb") as data:
-            blob.upload_blob(data, overwrite=True)
-        
-        return True
-
-    def _upload_path(self, container, src, dest):
-        basename = os.path.basename(src)
-        all_files = [file.name for file in Path(src).rglob('*') if file.is_file()]
-
-        for file in tqdm(all_files, desc='Upload'):
-            _src = os.path.join(src, file)
-            _dest = os.path.join(dest, basename, file)
-            
-            self._upload_file(container, _src, _dest)
-
-        return True
+        return job_info.__dict__['completed']
 
     ########### CHECK EXISTS ############
     def _exists(self, container, blob_name):
         return BlobClient.from_connection_string(conn_str=self.credential, container_name=container, blob_name=blob_name).exists()
 
     ########### DOWNLOAD ############
-    def _download_file(self, container, src, dest):
-        filename = os.path.join(dest, os.path.basename(src))
-        os.makedirs(os.path.dirname(filename), exist_ok=True)
-        blob = BlobClient.from_connection_string(conn_str=self.credential, container_name=container, blob_name=src)
-        blob.download_blob().readinto(open(filename, 'wb'))
+    def _download(self, container, src, dest):
+        transfer_option = AzCopyOptions(overwrite_existing=True, recursive=True)
+        job_info = self.az.download_data_to_local_location(src=src, dest=dest, transfer_options=transfer_option)
+        return job_info.__dict__['completed']
 
-        return True
-
-
-    def _download(self, container, src, dest, log=True):
-        dest = os.path.join(dest, os.path.basename(src))
-        blobs = self._list_blobs(container, src)
-
-        if not src.endswith('/'):
-            src += '/'
-
-        for blob in tqdm(blobs, desc='Download'):
-            # remove the src from the blob name
-            _src = blob.replace(src, '')
-            _dest = os.path.join(dest, os.path.dirname(_src))
-            self._download_file(container, blob, _dest)
-            
-        return True
-            
     ########### DELETE ############
     def _delete(self, container, path):
         blob = BlobClient.from_connection_string(conn_str=self.credential, container_name=container, blob_name=path)
